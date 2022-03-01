319b67286ebbc35c24d29d84596ef3d88b819563
ealisa818 <ealisa818@gmail.com>
2022-03-01 09:02:22 -0500

    add About

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Components/Nav';
+import About from './Components/About';
 import "./assets/style.css"
 
 
@@ -10,8 +11,24 @@ export default function App() {
     return (
      <div> 
          <Nav/>
+         <About/>
      </div>   
 
     
     )
 }
+// import Skills  from './components/Skills';
+// import Projects  from './components/Projects';
+// import Footer from './components/Footer';
+// import './assets/styles.css'
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Nav />
+//       <Hero />
+//       <AboutMe />
+//       <Skills />
+//       <Projects />
+//       <Footer />
+//     </div>
---
+++ my-app/src/Components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+export default function About() {
+    return (
+        <><div className="About">About</div><div className="writing"> After teaching English to speakers of other languages for over 15 years, I decided to make a career change to Web Development. Before teaching, I earned a degree in Actuarial Science from the University of Georgia. I am looking for an opportunity where I can use both my analytical thinking and problem solving skills. While teaching, I have always taken on leadership roles in the schools where I worked, including acting as teacher/administration liason, as well as being the district wide department leader. In Germany, together with a business partner, I started and ran Hamburg Language Lounge, a language school combined with a bar/café. I love learning about new cultures and studying languages. I am fluent in German.
+        </div></>    
+    )
+}
