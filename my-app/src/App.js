bee5b85e50307f484de5ca177e53c776a40ebdba
ealisa818 <ealisa818@gmail.com>
2022-03-01 01:08:34 -0500

    set up files

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './Components/Nav';
+
+
+
+
+
+export default function App() { 
+    return (
+     <div> 
+         <Nav/>
+     </div>   
+
+    
+    )
+}
